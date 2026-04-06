579d2c75276d1b0aa00b225bb135e529bfbf6e71
mherman22 <hermanmuhereza22@gmail.com>
2026-04-06 10:21:54 +0000

    Fix mpi-client identifier mapping and autoXref config
    - Change exportIdentitiferType key from "Patient ID" to "iSantePlus ID"
      to match the actual OpenMRS identifier type name in the DB
    - Add iSante ID mapping to exportIdentitiferType
    - Remove ECID from autoXref (generated by CR, not a local identifier)

--- projects/isanteplus-db/initial-db/isanteplus-db.sql
+++ projects/isanteplus-db/initial-db/isanteplus-db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef24c5192588f9a47ec372cb0e82855e968502744e96169fe8127c1e80328449
-size 82058406
+oid sha256:9826c6445a66d0460b865aaad9e946f8f6d8a0396aec5c3ef6c3ba4cb1cacff5
+size 82058424
